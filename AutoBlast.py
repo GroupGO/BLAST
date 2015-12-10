8b4ab843abe87cecc8f3034b20e2b716668ba309
GroupGO <henry.ehlers@wur.nl>
2015-12-10 14:55:30 +0100

    2015_12_10

--- AutoBlast.py
+++ AutoBlast.py
@@ -5,9 +5,14 @@
 Author: Henry Ehlers
 WUR_Number: 921013218060
 
-A script designed to ...
-    -input:
-    -output:
+A script designed to blast a variable number of FASTA-queries vs a variable number of databases.
+
+    Inputs:     [1] A string specifying the path to a text file containing the variable number
+                    of paths leading to the query FASTA files.
+                [2] A string specifying the path to a text file containing the variable number
+                    of paths leading to the BLAST databases to be used as references.
+                [3] A string specifying the path to the output directory in which the
+                    automatically named output files will be saved.
 
 In order to provide readable and understandable code, the right indentation margin has been
 increased from 79 to 99 characters, which remains in line with Python-Style-Recommendation (
@@ -23,9 +28,11 @@ import re
 
 def get_contents(path_text_file):
     """
+    Function to get the contents of a new-line delimited file and return its contents as a list
+    of strings.
 
-    :param path_text_file:
-    :return:
+    :param path_text_file: A string specifying the path of an input, new-line-delimited file.
+    :return: A list of strings containing the contents of the file.
     """
     paths = []
     with open(path_text_file) as text_file:
@@ -41,9 +48,9 @@ def run_blast(fasta_paths, genome_names, output_dir):
     Method to run blast n on multiple databases using multiple fasta-queries and save their
     output to a desired directory.
 
-    :param fasta_paths:
-    :param genome_names:
-    :return:
+    :param fasta_paths: A list of strings specifying the paths to the various FASTA files to be
+    used as queries.
+    :param genome_names: A list of strings specifying the paths to the various BLAST databases.
     """
     print('Running BLASTN.')
     current_path = os.getcwd()
@@ -62,6 +69,9 @@ def run_blast(fasta_paths, genome_names, output_dir):
 
 
 def main():
+    """
+    Method to blast a variable number of FASTA files vs a variable number of BLAST databases.
+    """
     fasta_paths, genome_names, output_directory = \
         get_command_line_arguments(['fasta_paths.txt', 'genome_paths.txt',
                                     '~/project/groups/go/Data/Blast_Data/Blast_Results/'])
